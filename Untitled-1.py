d1958a862600334b47d9cea7d0d38ace126d14b5
zhixi12 <90766000+zhixi12@users.noreply.github.com>
2021-09-15 18:58:40 +0800

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,14 @@
+def aa(*x):
+    print(x)
+x=['abcedf']
+z='edf'
+print(x[0][2:3])
+if x[3:5] == 'edf':
+    print(1)
+else:
+    print(0)
+#print(x.index(l))
+#for name in x:
+ #   print(name[1])
+
+
